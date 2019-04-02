94fc83f2a9fdc3f10fc8d190eca89b331ae96493
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-02 14:02:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,6 @@ end
 
 def monopoly
   monopoly = { :railroads => {}}
-
 end
 
 def monopoly_with_second_tier
@@ -25,11 +24,12 @@ def monopoly_with_second_tier
   monopoly = { :railroads => {
                 :pieces => 4, 
                 :names => {
+                  }
                 }
                 :rent_in_dollars => {
                 }
-    }
-  }
+              }
+            }
 end
 
 def monopoly_with_third_tier
