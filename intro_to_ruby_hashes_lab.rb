4f68ff6a654c9070b451dc8eb4fab1680c10d8a6
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-02 13:53:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,9 @@ def monopoly
 	monopoly = {:pieces => 4,
 	:names => {},
 	:rent_in_dollars => {},
-	  
+	:one_piece_owned => 25,  
+	:two_piece_owned => 50, 
+	:three_piece_owned => 100,
 	}
 
 end
